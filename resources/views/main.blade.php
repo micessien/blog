375506cdc229cc190c1e6776c27e0b591bde3416
micessien <micaeldie@gmail.com>
2021-02-16 10:07:57 +0000

    Add auto title in the main layout

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Laravel Blog</title> <!-- CHANGE THIS TITLE FOR EACH PAGE -->
+    <title>Laravel Blog | @yield('title')</title> <!-- CHANGE THIS TITLE FOR EACH PAGE -->
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
@@ -62,6 +62,9 @@
 
     <div class="container">
         @yield('content')
+
+        <hr />
+        <p class="text-center">Copyright Micael Die - All Right Reserved</p>
     </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
