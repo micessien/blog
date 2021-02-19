b4ba878318377dd68aec120015980d88a14c60ad
micessien <micaeldie@gmail.com>
2021-02-19 02:57:26 +0000

    Insert Post data and validating process

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -34,7 +35,20 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validate the data
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+        // store in the database
+        $post = new Post;
+
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+
+        // redirect to another page
+        return redirect()->route('posts.show', $post->id);
     }
 
     /**
