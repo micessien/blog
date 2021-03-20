863abdffece483b77a10344f2d8448a5585e89ec
micessien <micaeldie@gmail.com>
2021-03-20 09:44:00 +0000

    Styling our Comments

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -15,17 +15,28 @@
 
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
+        <h3 class="comment-title"><span class="glyphicon glyphicon-comment"></span> {{$post->comments->count()}}
+            Comments</h3>
         @foreach ($post->comments as $comment)
         <div class="comment">
-            <p><strong>Name:</strong> {{$comment->name}}</p>
-            <p><strong>Comment:</strong> <br> {{$comment->comment}}</p> <br>
+            <div class="author-info">
+                <img src="{{"https://www.gravatar.com/avatar/". md5(strtolower(trim($post->email))). "?s=50&d=monsterid"}}"
+                    alt="user image" class="author-image">
+                <div class="author-name">
+                    <h4>{{$comment->name}}</h4>
+                    <p class="author-time">{{date('F nS, Y - g:iA' ,strtotime($comment->created_at))}}</p>
+                </div>
+            </div>
+            <div class="comment-content">
+                {{$comment->comment}}
+            </div>
         </div>
         @endforeach
     </div>
 </div>
 
 <div class="row">
-    <div id="comment-form" class="col-md-8 col-md-offset-2" style="marging-top: 50px">
+    <div id="comment-form" class="col-md-8 col-md-offset-2" style="margin-top: 50px">
         {!! Form::open(['route'=>['comments.store', $post->id], 'method'=>'POST']) !!}
         <div class="row">
             <div class="col-md-6">
